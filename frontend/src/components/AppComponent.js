916e6229d1928a3e21ce4cc9a7b7061dc8b151f1
Emmanuel Maro <emmanuelmaro733@gmail.com>
2020-10-15 02:53:26 +0300

    Preparing React

--- frontend/src/components/AppComponent.js
+++ frontend/src/components/AppComponent.js
@@ -1,10 +1,28 @@
 import React, { Component} from "react";
+import { Route, Switch } from "react-router-dom";
+
+import LoginComponent from "./LoginComponent";
+import SignUpComponent from "./SignUpComponent";
 
 class AppComponent extends Component {
     render () {
         return (
             <div className="site">
-                <h1>Ahhh after 10,000 years I'm free. Time to conquer the Earth!</h1>
+                <nav>
+                    <Link className={"nav-link"} to={"/"}>Home</Link>
+                    <Link className={"nav-link"} to={"/login/"}>Login</Link>
+                    <Link className={"nav-link"} to={"/signup/"}>Signup</Link>
+                </nav>
+                
+                <main>
+                    <h1>Ahhh after 10,000 years I'm free. Time to conquer the Earth!</h1>
+                    
+                    <Switch>
+                        <Route exact path={"/login/"} component={LoginComponent}/>
+                        <Route exact path={"/signup/"} component={SignUpComponent}/>
+                        <Route path={"/"} render={() => <div>Home again</div>}/>
+                    </Switch>
+                </main>
             </div>
         );
     }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 import AppComponent from './components/AppComponent';
 
-render(<AppComponent  />, document.getElementById('root'));
+render((<BrowserRouter><AppComponent  /></BrowserRouter>), document.getElementById('root'));
