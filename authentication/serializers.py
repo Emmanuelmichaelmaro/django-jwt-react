d81e64ca62871d2549602f3e1c08eb7c0e3f5b51
Emmanuel Maro <emmanuelmaro733@gmail.com>
2020-10-14 12:51:10 +0300

    Registering a user

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,5 +1,7 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import serializers
+from .models import CustomUser
 
 # Create your serializers here.
 
@@ -19,3 +21,33 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
 class MyTokenObtainPairView(TokenObtainPairView):
     
     serializer_class = MyTokenObtainPairSerializer
+    
+class CustomUserSerializer(serializers.ModelSerializer):
+
+    """
+    Currently unused in preference of the below.
+    """
+    email = serializers.EmailField(required=True)
+    username = serializers.CharField()
+    password = serializers.CharField(min_length=8, write_only=True)
+
+    class Meta:
+
+        model = CustomUser
+
+        fields = ('email', 'username', 'password')
+
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+
+        password = validated_data.pop('password', None)
+
+        instance = self.Meta.model(**validated_data)  # as long as the fields are the same, we can just use this
+
+        if password is not None:
+            instance.set_password(password)
+
+        instance.save()
+
+        return instance
